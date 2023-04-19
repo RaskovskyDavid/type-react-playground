6385fcc0d96e8f12ba2091454689def0a0c1ac23
david <davidtucu@hotmail.com>
2023-04-19 14:06:57 -0300

    como agregar una nueva prop

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,14 +1,16 @@
 interface ChildProps {
     color: string;
-    // onClick: () => void;
+    onClick: () => void;
     // children?: React.ReactNode;
 }
 
-export const Child = ({color}: ChildProps) => {
-    return <div>{color}</div>
+export const Child = ({color, onClick}: ChildProps) => {
+    return <div>{color}
+    <button onClick={onClick}>Clic kMe</button>
+    </div>
 };
 
-export const ChildAsFc: React.FC<ChildProps> =({color}) =>{
-    return <div>{color}</div>;
+export const ChildAsFc: React.FC<ChildProps> =({color, onClick}) =>{
+    return <div>{color}<button onClick={onClick}>Clic kMe</button></div>;
 };
 
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,5 +1,5 @@
 import { Child } from './Child';
 const Parent = () => {
-    return <Child color='white' />
+    return <Child color='white' onClick={()=> console.log('click')} />
 };
 export default Parent;
