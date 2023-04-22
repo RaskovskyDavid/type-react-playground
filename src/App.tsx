5aa797ab5b7c76e89b0fa9635b0dc01db32ef0b0
david <davidtucu@hotmail.com>
2023-04-22 16:30:31 -0300

    eventos y su forma de manejarla dentro de los componentes, pasandolos en funciones

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import GuestList from './state/GuestList';
 import UserSearch from './state/UserSearch';
+import EventComponent from './events/EventComponent';
 
 function App() {
   return (
     <div className="App">
       
       {/* <GuestList /> */}
-      <UserSearch />
+      {/* <UserSearch /> */}
+      <EventComponent />
     </div>
   );
 }
